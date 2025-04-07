1f1eab2bced538d6bb97440a97a1d36e72d9cee4
jacob-s-codes <jacobbshaul@gmail.com>
2025-04-07 15:48:20 -0700

    improved home screen

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -119,7 +119,7 @@ const Navbar = () => {
                                 <div className="container mx-auto w-full flex flex-wrap lg:flex-nowrap justify-center items-center ">
                                     {/* Text Section - 50% width */}
                                     <div className="flex flex-col items-center justify-center w-full lg:w-1/2 px-4 ">
-                                        <ul className="w-full  pb-6 pt-6 border border-white px-2 rounded-lg">
+                                        <ul className="w-full  pb-6 pt-6 border-2 hover:bg-darkbg2 border-white px-2 rounded-lg">
                                             <div className="flex items-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-book h-8 mb-3 mr-3 fill-current text-white" viewBox="0 0 16 16">
                                                     <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
@@ -129,7 +129,7 @@ const Navbar = () => {
                                             <p className="text-sm text-white">Check out the amazing volunteers, teachers, and leaders that help Mode to Code!</p>
                                             <Learnmorebtn />
                                         </ul>
-                                        <ul className="w-full mt-2 pb-6 pt-6 border border-white px-2 rounded-lg">
+                                        <ul className="w-full mt-2 pb-6 pt-6 border-2 hover:bg-darkbg2 border-white px-2 rounded-lg">
                                             <div className="flex items-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-book h-8 mb-3 mr-3 fill-current text-white" viewBox="0 0 16 16">
                                                     <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
@@ -139,7 +139,7 @@ const Navbar = () => {
                                             <p className="text-sm text-white">Check out the amazing volunteers, teachers, and leaders that help Mode to Code!</p>
                                             <Learnmorebtn />
                                         </ul>
-                                        <ul className="w-full mt-2 pb-6 pt-6 border border-white px-2 rounded-lg">
+                                        <ul className="w-full mt-2 pb-6 pt-6 border-2 border-white hover:bg-darkbg2 px-2 rounded-lg">
                                             <div className="flex items-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" className="bi bi-book h-8 mb-3 mr-3 fill-current text-white" viewBox="0 0 16 16">
                                                     <path d="M1 2.828c.885-.37 2.154-.769 3.388-.893 1.33-.134 2.458.063 3.112.752v9.746c-.935-.53-2.12-.603-3.213-.493-1.18.12-2.37.461-3.287.811zm7.5-.141c.654-.689 1.782-.886 3.112-.752 1.234.124 2.503.523 3.388.893v9.923c-.918-.35-2.107-.692-3.287-.81-1.094-.111-2.278-.039-3.213.492zM8 1.783C7.015.936 5.587.81 4.287.94c-1.514.153-3.042.672-3.994 1.105A.5.5 0 0 0 0 2.5v11a.5.5 0 0 0 .707.455c.882-.4 2.303-.881 3.68-1.02 1.409-.142 2.59.087 3.223.877a.5.5 0 0 0 .78 0c.633-.79 1.814-1.019 3.222-.877 1.378.139 2.8.62 3.681 1.02A.5.5 0 0 0 16 13.5v-11a.5.5 0 0 0-.293-.455c-.952-.433-2.48-.952-3.994-1.105C10.413.809 8.985.936 8 1.783" />
@@ -153,8 +153,7 @@ const Navbar = () => {
 
                                     {/* Image Section - 50% width */}
                                     <div className="w-full lg:w-1/2 px-4 flex flex-col items-center justify-center ">
-                                        <img src="/buildwebsites.jpg" alt="MTC Logo" className="w-auto h-64 object-contain rounded-lg" />
-                                        <img src="/aibg.jpeg" alt="MTC Logo" className="w-auto h-64 object-contain rounded-lg" />
+                                        <img src="/aibg.jpeg" alt="MTC Logo" className="w-full h-auto object-contain rounded-lg" />
                                     </div>
                                 </div>
                             </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,14 @@ import Navbar from "./components/Navbar";
 export default function Home() {
   return (
     <div>
-      <Navbar />
-      <div className="my-[100vh]">
-        Hello world
+      <div className="homebg h-screen w-full bg-cover bg-center">
+        <div className="h-full flex flex-col justify-center items-center text-white">
+          <h1 className="text-9xl font-bold text-center">MODE TO CODE</h1>
+          <h2 className="text-6xl pt-6 text-center">Teaching students of all ages</h2>
+        </div>
+      </div>
+      <div className="mt-[90vh]">
+        Hi
       </div>
     </div>
   );
